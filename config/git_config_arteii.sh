2e1cfd348da5698b84850d06bb0398b5d836fcf8
Arteii <ben.arteii@proton.me>
2024-08-30 17:48:15 +0200

    dded git config (arteii), added gitignore

---
+++ config/git_config_arteii.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name "Arteii"
+git config --global user.email ben.arteii@proton.me
+
+git config --global core.editor nvim
+
+git config --global init.defaultBranch main
+
+git config --list
+
